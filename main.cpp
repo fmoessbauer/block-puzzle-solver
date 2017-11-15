185f337ed270aecdbe116f55267ffdd8ef6e78ec
Felix Moessbauer <felix.moessbauer@gmail.com>
2017-11-15 22:08:16 +0100

    finished implementation of non-async search

--- main.cpp
+++ main.cpp
@@ -11,22 +11,39 @@
  */
 
 #include <vector>
+#include <unordered_set>
 #include <array>
 #include <bitset>
 #include <algorithm>
 #include <iostream>
+#include <chrono>
+#include <future>
 
 #include <blaze/Blaze.h>
 
 constexpr std::array<size_t, 3> cube_ext{{3UL,2UL,2UL}};
+//constexpr std::array<size_t, 3> cube_ext{{5UL,5UL,5UL}};
 
-using stone_l  = std::integral_constant<size_t, 3UL>;
+//using stone_l  = std::integral_constant<size_t, 3UL>;
+using stone_l  = std::integral_constant<size_t, 5UL>;
 using mask_t   = blaze::StaticMatrix<int, 3UL, stone_l::value>;
 using offset_t = blaze::StaticMatrix<int, 3UL, 1UL>;
 using rot_t    = blaze::StaticMatrix<int, 3UL, 3UL>;
 using bitset_t = std::bitset<std::get<0>(cube_ext) *
                              std::get<1>(cube_ext) *
                              std::get<2>(cube_ext)>;
+
+struct state {
+  // index, stone
+  using rem_t = std::pair<int, bitset_t>;
+
+  bitset_t           cube;
+  std::vector<rem_t> remaining;
+  std::vector<int>   path;
+  unsigned long long processed;
+  unsigned long long processed_last;
+  std::chrono::time_point<std::chrono::system_clock> lasthit;
+};
 
 const rot_t rot_x{{1,0,0}, {0,0,-1},{0,1,0}};
 const rot_t rot_y{{0,0,1}, {0,1,0}, {-1,0,0}};
@@ -70,7 +87,6 @@ mask_t stone_to_block(mask_t matrix){
 }
 
 bitset_t block_to_bitmask(const mask_t & block){
-  const size_t numrows = block.rows(); // 3
   const size_t numcols = block.columns(); // n
 
   bitset_t bitset;
@@ -78,27 +94,13 @@ bitset_t block_to_bitmask(const mask_t & block){
     auto col = blaze::column(block, i);
     bitset.set(col[0]
              + col[1] * std::get<0>(cube_ext)
-             + col[2] * std::get<1>(cube_ext) * std::get<2>(cube_ext));
+             + col[2] * std::get<0>(cube_ext) * std::get<1>(cube_ext));
   }
   return bitset;
 }
 
-int num_unique_permutations(const std::vector<mask_t> & blocks){
-  std::vector<bitset_t> bitmasks;
-  std::transform(blocks.begin(), blocks.end(),
-                 std::back_inserter(bitmasks), block_to_bitmask);
-
-  std::vector<unsigned long long> bits;
-  std::transform(bitmasks.begin(), bitmasks.end(),
-                 std::back_inserter(bits), [](const auto & m){return m.to_ullong();});
-
-  std::sort(bits.begin(), bits.end());
-  bits.erase(std::unique(bits.begin(), bits.end()), bits.end());
-  return bits.size();
-}
-
 std::vector<bitset_t> filter_bitmasks(const std::vector<bitset_t> & bitsets){
-  using comp_t = std::pair<unsigned int, unsigned long long>;
+  using comp_t = std::pair<unsigned int, std::string>;
   std::vector<comp_t> bitmasks;
   bitmasks.reserve(bitsets.size());
   // pair bitmasks with index
@@ -106,7 +108,7 @@ std::vector<bitset_t> filter_bitmasks(const std::vector<bitset_t> & bitsets){
   std::transform(bitsets.begin(), bitsets.end(),
                  std::back_inserter(bitmasks),
                  [&i](const auto & m){
-                  return std::make_pair(i++,m.to_ullong());});
+                  return std::make_pair(i++,m.to_string());});
   std::sort(bitmasks.begin(), bitmasks.end(),
       [&bitmasks](const auto & a, const auto & b){
         return a.second < b.second;
@@ -127,9 +129,172 @@ std::vector<bitset_t> filter_bitmasks(const std::vector<bitset_t> & bitsets){
   return result;
 }
 
+inline bool valid_combination(bitset_t a, bitset_t b){
+  a.flip();
+  b.flip();
+  a |= b;
+  return a.all();
+}
+
+// TODO
+bool layer_check(const bitset_t & cube){
+  static std::vector<bitset_t> layer_masks;
+
+  // cache masks if necessary
+  if(layer_masks.size() == 0){
+    bitset_t mask;
+    for(unsigned int l=0; l<cube_ext[2]; ++l){
+      unsigned int layer_size = cube_ext[0] * cube_ext[1];
+      int begin = layer_size * l;
+      int end   = layer_size * (l+1);
+      for(unsigned i=begin; i<end; ++i){
+        mask.set(i);
+      }
+    }
+    layer_masks.push_back(mask);
+  }
+//  for(unsigned int l=0; l<cube_ext[2]; ++l){
+//    cube
+//  }
+}
+
+bool solve_puzzle_impl(
+    int depth,
+    state & state,
+    int begin,
+    int end){
+  // reduce cache misses
+  decltype(state.remaining) local_remaining(state.remaining);
+
+  for(int i=begin; i<end; ++i){
+    auto & pos = local_remaining[i];
+    // range is ordered, hence end at first "used" bitmask
+    if(std::abs(pos.first) < 0){continue;};
+    if(valid_combination(pos.second, state.cube)){
+      ++state.processed;
+      state.cube|=pos.second;
+      // append index to path
+      state.path.push_back(pos.first);
+      // remove from freelist
+      pos.first*=(-1);
+//      std::cout << "Try valid stone:" << pos.second
+//                << " Result: " << state.cube
+//                << " Depth: " << depth << std::endl;
+
+      if(depth == 0){return true;}
+      if(solve_puzzle_impl(depth-1, state, 0, local_remaining.size())){return true;}
+
+      //backtrack
+      if(depth >= 22){
+        auto now  = std::chrono::system_clock::now();
+        auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(now - state.lasthit);
+        //if(state.processed >= 17189084125){
+        //  exit(0);
+        //}
+        if(diff.count() > 1000){
+          auto keys_processed   = state.processed - state.processed_last;
+          double mkeys_per_sec  = static_cast<double>(keys_processed / diff.count()) / 1000.0;
+          std::cout << "Backtrack at depth:" << depth
+                    << ", Remaining:" << state.remaining.size()
+                    << ", Processed:" << state.processed 
+                    << ", MKey/s:"    << mkeys_per_sec 
+                    << ", Cube:" << state.cube << std::endl;
+          state.lasthit = now;
+          state.processed_last = state.processed;
+        }
+      }
+      state.cube^=pos.second;
+      pos.first*=(-1);
+      state.path.pop_back();
+    }
+  }
+  return false;
+}
+
+bool solve_puzzle(int depth, state & st){
+  std::vector<std::future<bool>> tasks;
+  int num_tasks    = 1;
+  int problem_size = st.remaining.size();
+  int task_size    = problem_size / num_tasks;
+  std::vector<state> states(num_tasks);
+
+//  for(unsigned int i=0; i<num_tasks; ++i){
+//    int begin = task_size*i;
+//    int end   = (i==num_tasks-1) ? problem_size : task_size*(i+1);
+//    states[i] = st;
+//    tasks.emplace_back(
+//        std::async(
+//          std::launch::async,
+//          solve_puzzle_impl, depth, states[i], begin, end));
+//  }
+//  for(auto & t : tasks){
+//    t.wait();
+//  }
+//  for(unsigned int i=0; i<num_tasks; ++i){
+//    bool result = tasks[i].get();
+//    if(result){
+//      st.path = states[i].path;
+//      return true;
+//    }
+//  }
+//  return false;
+  return solve_puzzle_impl(depth, st, 0, problem_size);
+}
+
+
+/**
+ * Converts solution given as a path with indices shifted by one
+ * to a cube of indices where each position in the cube is
+ * marked with the path-index of the covering stone.
+ */
+std::vector<int> cube_of_indices(
+    const std::vector<int> & path,
+    const std::vector<bitset_t> & all_masks)
+{
+  size_t size = all_masks[0].size();
+  std::vector<int> cube(size);
+
+  for(unsigned int x=0; x<path.size(); ++x){
+    const auto & s = all_masks[path[x]-1];
+    for(unsigned int i=0; i<size; ++i){
+      if(s.test(i)){
+        cube[i] = x; 
+      }
+    }
+  }
+  return cube;
+}
+
+template<typename It>
+void shuffle_range(It a, It b){
+  std::random_device rd;
+  std::mt19937 g(rd());
+       
+  std::shuffle(a, b, g);
+}
+
+void print_cube_slices(
+    const std::vector<int> & cube)
+{
+  const int splits_row   = std::get<0>(cube_ext);
+  const int splits_layer = std::get<0>(cube_ext) * std::get<1>(cube_ext);
+  const std::string delim(splits_row*3-1, '=');
+
+  std::cout << delim << std::endl;
+  for(unsigned int i=0; i<cube.size(); ++i){
+    std::cout << std::setw(2)
+              << cube[i] << " ";
+    if(i % splits_row == (splits_row-1)){
+      std::cout << std::endl;
+    }
+    if(i % splits_layer == (splits_layer-1)){
+      std::cout << delim << std::endl;
+    }
+  }
+}
+
 int main(int argc, char** argv){
   std::vector<mask_t> blocks;
-  auto s = stone;
   blaze::IdentityMatrix<int> eye(3UL);
   rot_t rx = eye;
   for(int i=0; i<4; ++i){
@@ -155,12 +320,12 @@ int main(int argc, char** argv){
   std::transform(blocks.begin(), blocks.end(),
                  std::back_inserter(bitmasks), block_to_bitmask);
 
+  filter_bitmasks(bitmasks);
+
   std::cout << "Bitmasks:" << std::endl;
   for(const auto & mask : bitmasks){
     std::cout << mask << std::endl;
   }
-
-  std::cout << "Unique Bitmasks: " << num_unique_permutations(blocks) << std::endl;
 
   std::cout << "Move Stones:" << std::endl;
   std::vector<mask_t> all_pos_blocks;
@@ -168,6 +333,7 @@ int main(int argc, char** argv){
     std::array<int, 3> move_steps;
     for(int d=0; d<3; ++d){
       move_steps[d] = cube_ext[d] - minmax_dim(block, d).second;
+//      std::cout << "Move steps: " << d << "," << move_steps[d] << std::endl;
     }
     for(int x=0; x<move_steps[0]; ++x){
       for(int y=0; y<move_steps[1]; ++y){
@@ -185,13 +351,35 @@ int main(int argc, char** argv){
                  std::back_inserter(all_pos_masks), block_to_bitmask);
 
 
-  for(const auto & mask : all_pos_masks){
-    std::cout << mask << std::endl;
-  }
-
   std::cout << "Bitmasks: " << all_pos_masks.size() << std::endl;
   auto unique_pos_masks = std::move(filter_bitmasks(all_pos_masks));
   std::cout << "Unique Bitmasks: " << unique_pos_masks.size() << std::endl;
+
+  for(unsigned int i=0; i<unique_pos_masks.size(); ++i){
+    std::cout << unique_pos_masks[i] << ", " << i << std::endl;
+  }
+
+  // prepare state for solving puzzle
+  state st;
+  int index=0;
+  // append index+1 to each mask
+  std::transform(unique_pos_masks.begin(), unique_pos_masks.end(),
+                 std::back_inserter(st.remaining),
+                 [&index](const auto & m){
+                   return std::make_pair(++index, m);
+                 });
+
+  if(solve_puzzle(3, st)){
+    std::cout << "Found solution: " << std::endl;
+    for(const int e : st.path){
+      std::cout << (e-1) << ",";
+    }
+    std::cout << std::endl;
+    auto solved_cube = cube_of_indices(st.path, unique_pos_masks);
+    print_cube_slices(solved_cube);
+  } else{
+    std::cout << "Found NO solution" << std::endl;
+  }
 
   return 0;  
 }
